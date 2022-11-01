2785c8802627329c5a2b2eb68a3e6564aa26c2fd
andrew-kol <110204316+andrew-kol@users.noreply.github.com>
2022-11-01 21:42:41 +0300

    commit2

--- Main.java
+++ Main.java
@@ -1,2 +1,35 @@
+package hw3.Task3;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+
+    static Passport passport1 = new Passport("1234 123456", "Иванов", "Иван", "Иванович", "10.11.1973");
+    static Passport passport2 = new Passport("6541 123456", "Петров", "Петр", "Петрович", "10.08.1985");
+    static Passport passport3 = new Passport("9876 123456", "Сидоров", "Сидор", "Сидорович", "01.12.1981");
+    static Passport passport4 = new Passport("1234 123456", "Иванов", "Иван", "Иванович", "10.11.1973");
+
+    static Map<String, Passport> map = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        addPassport(passport1);
+        addPassport(passport2);
+        addPassport(passport3);
+        addPassport(passport4);
+
+        System.out.println(map);
+        System.out.println(getPassport("1234 123456"));
+    }
+
+    public static void addPassport(Passport passport) {
+        map.put(passport.getNumber(), passport);
+    }
+
+    public static Passport getPassport(String number) {
+        return map.get(number);
+
+    }
 }
